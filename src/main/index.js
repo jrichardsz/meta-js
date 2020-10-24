133f50c19cd8bafb71995a851d41a48fb3719de5
jrichardsz <jrichardsz.java@gmail.com>
2020-10-24 11:12:08 -0500

    expose AnnotationHelper

--- src/main/index.js
+++ src/main/index.js
@@ -2,7 +2,9 @@
 const include = require('nodejs-require-enhancer');
 const NodeInternalModulesHook = include('org/metajs/hook/NodeInternalModulesHook.js');
 const DependencyHelper = include('org/metajs/core/DependencyHelper.js');
+const AnnotationHelper = include('org/metajs/core/AnnotationHelper.js');
 const MetaJsContextHelper = include('org/metajs/context/MetaJsContextHelper.js');
 exports.NodeInternalModulesHook = NodeInternalModulesHook;
 exports.DependencyHelper = DependencyHelper;
 exports.MetaJsContextHelper = MetaJsContextHelper;
+exports.AnnotationHelper = AnnotationHelper;
