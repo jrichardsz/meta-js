714fb590b767847b8f8ef9a8e1b3438e61010853
Richard Osmar Leon Ingaruca <jrichardsz.java@gmail.com>
2021-02-03 17:43:20 -0500

    Fixed issue when two annotations are in the same Js Module

--- src/main/org/metajs/hook/NodeInternalModulesHook.js
+++ src/main/org/metajs/hook/NodeInternalModulesHook.js
@@ -3,16 +3,8 @@ NodeInternalModulesHook._compile = function() {
   var Module = require('module');
   var original_compile = Module.prototype._compile;
   Module.prototype._compile = function(content, filename) {    
-    //TODO: avoid iteration with simple content.replace(/^\s*@/g, '//@');
-    // content = content.replace(/.*@/g, '//@');
-    // content = content.replace(/^\s*@/g, '//@');
-    // content = content.replace(/@/g, '//@');    
-    var annotationMatchs = content.match(/@[A-Z]\w+/g);
-    if(annotationMatchs != null && typeof annotationMatchs[Symbol.iterator] === 'function' ){
-      for(let annotationMatch of annotationMatchs ){
-        content = content.replace(annotationMatch, '//'+annotationMatch);  
-      }      
-    }
+    //TODO: use more complex regex to detect annotations
+    content = content.replace(/(@[A-Z]\w+)/g, "//$1");
     return original_compile.call(this, content, filename);
   };
 }
